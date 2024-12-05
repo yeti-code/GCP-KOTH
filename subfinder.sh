0a13c29f0a5cc5b37fee4df986222442ece679e3
Yeti <76129522+yeti-code@users.noreply.github.com>
2024-12-05 15:52:49 -0600

    Update subfinder.sh

--- subfinder.sh
+++ subfinder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 subfinder -up
 
-readarray -t lines < "/home/joshb/Desktop/GCP-Genie/resources/targets.txt"
+readarray -t lines < "/home/User/Desktop/GCP-Genie/resources/targets.txt"
 
 for line in "${lines[@]}"; do
-    ~/go/bin/subfinder -provider-config /"$HOME"/.config/subfinder/provider-config.yaml -d "$line" -o /home/joshb/Desktop/GCP-Genie/resources/outputs/target_list_subdomains.txt
+    ~/go/bin/subfinder -provider-config /"$HOME"/.config/subfinder/provider-config.yaml -d "$line" -o /home/User/Desktop/GCP-Genie/resources/outputs/target_list_subdomains.txt
 done
