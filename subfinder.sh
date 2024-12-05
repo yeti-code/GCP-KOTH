70c7161971baafc0de6d59db3c6a76fffa89a9d0
Yeti <76129522+yeti-code@users.noreply.github.com>
2024-12-05 15:55:12 -0600

    Update subfinder.sh

--- subfinder.sh
+++ subfinder.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script takes a target list from a file and reads them line by line, running a custom subfinder command that queries for subdomains. Which will be the final list of potential targets for DNS subdomain takeover.
 subfinder -up
 
 readarray -t lines < "/home/User/Desktop/GCP-Genie/resources/targets.txt"
